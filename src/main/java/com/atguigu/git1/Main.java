86b85f965c76d61d86f243be630b3fc44e44746c
haerchuan <2902744383@qq.com>
2024-04-11 01:19:39 +0800

    fist66

--- src/main/java/com/atguigu/git1/Main.java
+++ src/main/java/com/atguigu/git1/Main.java
@@ -12,5 +12,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!222222");
         System.out.println("555555555");
+        System.out.println("4444444444");
     }
 }
