89873e7e653eb6bbd693116723804eef28240871
哈尔船长 <90689872+haerchuan@users.noreply.github.com>
2024-04-11 01:45:04 +0800

    Update Main.java

--- src/main/java/com/atguigu/git1/Main.java
+++ src/main/java/com/atguigu/git1/Main.java
@@ -6,10 +6,7 @@ package com.atguigu.git1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!222222");
-        System.out.println("555555555");
-        System.out.println("4444444444");
-        System.out.println("7777777777777");
-        System.out.println("zzzzzzzzzzzzzz");
+        System.out.println("Hello world!222222111111111111111");
+  
     }
 }
