3ba2996aeecf8f74651c1c3091809dc86d06aafc
haerchuan <2902744383@qq.com>
2024-04-11 00:47:21 +0800

    fist1

---
+++ src/main/java/com/atguigu/git1/Main.java
@@ -0,0 +1,15 @@
+package com.atguigu.git1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author xmy
+ * @Date 2024/4/11
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
