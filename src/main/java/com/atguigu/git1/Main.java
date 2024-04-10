c8885c56122740d79f443c6034eb6c3a923b28b4
haerchuan <2902744383@qq.com>
2024-04-11 01:08:56 +0800

    fist22

--- src/main/java/com/atguigu/git1/Main.java
+++ src/main/java/com/atguigu/git1/Main.java
@@ -11,6 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!222222");
-        System.out.println("33333333");
+        System.out.println("555555555");
     }
 }
